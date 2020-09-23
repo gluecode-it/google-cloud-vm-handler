bbf07f5bee35a5a846c42d2e3da918674ac52488
emi190396 <emily.lorenz@oliverlorenz.com>
2020-09-23 11:14:15 +0200

    chore: initial version

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,23 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 0
+    },
+  },
   coverageDirectory: "coverage",
-  moduleFileExtensions: ["js", "ts"],
+
+  moduleFileExtensions: [
+    "js",
+    //   "json",
+    //   "jsx",
+    "ts",
+    //   "tsx",
+    //   "node"
+  ],
+
   roots: ["src"],
   testEnvironment: "node",
 };
